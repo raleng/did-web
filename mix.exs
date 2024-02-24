0be0825a34a1529394594becd759a13f09ecfd50
Ralf Engbers <ralf.engbers@mattr.global>
2024-02-24 16:50:15 +1300

    Remove example dependencies

--- mix.exs
+++ mix.exs
@@ -22,8 +22,6 @@ defmodule DidWeb.MixProject do
   defp deps do
     [
       {:req, "~> 0.4.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
