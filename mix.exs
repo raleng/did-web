bd49fa3cf2b6739294e20fbcf6d1162a3eb46767
raleng <github@ralfengbers.com>
2024-03-16 21:31:23 +1300

    Increase version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DidWeb.MixProject do
   def project do
     [
       app: :did_web,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       description: description(),
